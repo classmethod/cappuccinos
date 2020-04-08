dd1e60e7ae2ae007e386762c52073cb63f8baa0d
SHUJI Watanabe <watanabe.shuji@classmethod.jp>
2020-04-08 16:55:49 +0900

    types 追加

---
+++ lib/types.d.ts
@@ -0,0 +1,17 @@
+
+export interface ProjectConfig {
+    name: string;
+    layers: LayersConfig[];
+}
+
+export interface LayersConfig {
+    name: string;
+    build: string[];
+    files: IFile[];
+}
+
+export interface IFile {
+    base_dir: string;
+    source: string;
+    destination: string;
+}
