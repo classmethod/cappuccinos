37a4bbde7d9facf6e514fcd9cff98f92ea01eca6
SHUJI Watanabe <watanabe.shuji@classmethod.jp>
2020-04-08 20:19:07 +0900

    newFunctions 追加

--- lib/Cappuccinos.ts
+++ lib/Cappuccinos.ts
@@ -1,36 +1,67 @@
 import * as AWS from 'aws-sdk';
 import { Layers } from './Layers';
-import { ProjectConfig } from './types';
+import { Functions } from './Functions';
+import { AwsConfig } from './types';
 import * as utils from './utils';
-import { green, red } from 'colorette';
+import { green } from 'colorette';
 
 export const newLayers = async (env: string, options: any, logger: any): Promise<Layers> => {
-    logger.debug(`[Load] project.yaml`);
-    logger.info(`ENV: ${green(env)}`);
-    const config = await utils.loadProjectConfig(env);
-    logger.debug(JSON.stringify(config, null, 2));
-    if (!options.ignoreProfile) {
-        const awsProfile = await utils.getAwsProfile(env);
-        logger.info(`AWS_PROFILE: ${green(awsProfile)}`);
-        AWS.config.credentials = new AWS.SharedIniFileCredentials({
-            profile: awsProfile
-        });
+    const cap = new Cappuccinos(env, options, logger);
+    const config = await cap.loadProjectConfig();
+    await cap.initAwsSdk();
+    return new Layers(env, options, logger, config);
+}
+
+export const newFunctions = async (env: string, options: any, logger: any): Promise<Functions> => {
+    const cap = new Cappuccinos(env, options, logger);
+    const config = await cap.loadProjectConfig();
+    await cap.initAwsSdk();
+    return new Functions(env, options, logger, config);
+}
+/*
+        const accountId = await (async () => {
+            const ret = await new AWS.STS().getCallerIdentity({}).promise();
+            this.logger.debug(JSON.stringify(ret, null, 2));
+            if (ret.Account === undefined) throw new Error('AccountId is unknown.');
+            return ret.Account;
+        })();
+        this.logger.info(`AWS_ACCOUNT_ID: ${green(accountId)}`);
+        if (AWS.config.region === undefined) throw new Error('Region is unknown.');
+*/
+
+class Cappuccinos {
+    env: string;
+    options: any;
+    logger: any;
+
+    constructor(env: string, options: any, logger: any) {        
+        this.logger = logger;
+        this.env = env;
+        this.options = options;
     }
-    AWS.config.update({
-        maxRetries: 15,
-        retryDelayOptions: {
-            customBackoff: retryCount => (Math.random() * 700 + 300)
+
+    async loadProjectConfig() {
+        this.logger.debug(`[Load] project.yaml`);
+        this.logger.info(`ENV: ${green(this.env)}`);
+        const config = await utils.loadProjectConfig(this.env);
+        this.logger.debug(JSON.stringify(config, null, 2));
+        return config;
+    }
+
+    async initAwsSdk(): Promise<AwsConfig | undefined> {
+        const awsConfig = await utils.getAwsConfig(this.env);
+        if (!this.options.ignoreProfile && awsConfig) {
+            this.logger.info(`AWS_PROFILE: ${green(awsConfig.aws_profile)}`);
+            AWS.config.credentials = new AWS.SharedIniFileCredentials({
+                profile: awsConfig.aws_profile
+            });
         }
-    });
-    const accountId = await (async () => {
-        const ret = await new AWS.STS().getCallerIdentity({}).promise();
-        logger.debug(JSON.stringify(ret, null, 2));
-        if (ret.Account === undefined) throw new Error('AccountId is unknown.');
-        return ret.Account;
-    })();
-    logger.info(`AWS_ACCOUNT_ID: ${green(accountId)}`);
-    if (AWS.config.region === undefined) throw new Error('Region is unknown.');
-    const region = AWS.config.region;
-    logger.info(`AWS_REGION: ${green(region)}`);
-    return new Layers(env, options, logger, config, accountId);
+        AWS.config.update({
+            maxRetries: 15,
+            retryDelayOptions: {
+                customBackoff: retryCount => (Math.random() * 700 + 300)
+            }
+        });
+        return awsConfig;
+    }
 }
