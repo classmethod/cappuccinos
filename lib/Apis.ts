5dde4e689e814c53ea291aeb3eee59a1602660b5
SHUJI Watanabe <watanabe.shuji@classmethod.jp>
2020-12-02 19:13:40 +0900

    format

--- lib/Apis.ts
+++ lib/Apis.ts
@@ -10,215 +10,215 @@ import { CappuccinosBase } from './CappuccinosBase'
 
 export class Apis extends CappuccinosBase {
 
-    constructor(env: string, options: any, logger: any, config: ProjectConfig, awsConfig: AwsConfig) {
-        super(env, options, logger, config, awsConfig, './build/apis');
-    }
+  constructor(env: string, options: any, logger: any, config: ProjectConfig, awsConfig: AwsConfig) {
+    super(env, options, logger, config, awsConfig, './build/apis');
+  }
 
-    async cleanup() {
-        await utils.cleanupDir(this.buidDir);
-        this.logger.info(`  # cleanup`);
-    }
+  async cleanup() {
+    await utils.cleanupDir(this.buidDir);
+    this.logger.info(`  # cleanup`);
+  }
 
-    async makeDocumentAll() {
-        await Promise.all(
-            this.projectConfig.apis.map(apiName => this.makeDocument(apiName))
-        );
-    }
+  async makeDocumentAll() {
+    await Promise.all(
+      this.projectConfig.apis.map(apiName => this.makeDocument(apiName))
+    );
+  }
 
-    async makeDocument(apiName: string) {
-        const yaml = await this.mergeSwaggerFile(apiName);
-        const path = `${this.buidDir}/${apiName}.swagger.yaml`;
-        writeFileSync(path, YAML.stringify(yaml));
-        this.logger.debug(yaml);
-        const out = execSync(`spectacle -t ${this.buidDir}/${apiName} ${path}`);
-        this.logger.debug(out.toString());
-        this.logger.info(`  # API document created        ${blue('api=')}${apiName}`);
-    }
+  async makeDocument(apiName: string) {
+    const yaml = await this.mergeSwaggerFile(apiName);
+    const path = `${this.buidDir}/${apiName}.swagger.yaml`;
+    writeFileSync(path, YAML.stringify(yaml));
+    this.logger.debug(yaml);
+    const out = execSync(`spectacle -t ${this.buidDir}/${apiName} ${path}`);
+    this.logger.debug(out.toString());
+    this.logger.info(`  # API document created        ${blue('api=')}${apiName}`);
+  }
 
-    async mergeSwaggerFile(apiName: string) {
-        const swg = utils.loadYaml(`apis/${apiName}/swagger.yaml`);
-        if (swg.paths === undefined) swg.paths = {};
-        glob(`functions/${apiName}/**/api.yaml`).map(path => {
-            const paths = utils.loadYaml(path);
-            Object.keys(paths).map(key => {
-                if (swg.paths[key] === undefined) swg.paths[key] = {};
-                Object.keys(paths[key]).map(method => {
-                    swg.paths[key][method] = paths[key][method];
-                });
-            });
+  async mergeSwaggerFile(apiName: string) {
+    const swg = utils.loadYaml(`apis/${apiName}/swagger.yaml`);
+    if (swg.paths === undefined) swg.paths = {};
+    glob(`functions/${apiName}/**/api.yaml`).map(path => {
+      const paths = utils.loadYaml(path);
+      Object.keys(paths).map(key => {
+        if (swg.paths[key] === undefined) swg.paths[key] = {};
+        Object.keys(paths[key]).map(method => {
+          swg.paths[key][method] = paths[key][method];
         });
-        if (swg.definitions === undefined) swg.definitions = {};
-        glob(`apis/${apiName}/definitions/*.yaml`).map(path => {
-            const definitions = utils.loadYaml(path);
-            Object.keys(definitions).map(key => {
-                swg.definitions[key] = definitions[key];
-            });
-        });
-        return swg;
-    }
+      });
+    });
+    if (swg.definitions === undefined) swg.definitions = {};
+    glob(`apis/${apiName}/definitions/*.yaml`).map(path => {
+      const definitions = utils.loadYaml(path);
+      Object.keys(definitions).map(key => {
+        swg.definitions[key] = definitions[key];
+      });
+    });
+    return swg;
+  }
 
-    async deploy() {
-        await this.uploadSwaggerFiles();
-        const stackName = this.getStackName();
-        this.logger.debug(stackName);
-        const stack = await this.describeStack(stackName);
-        if (stack === undefined) {
-            await this.createStack(stackName, this.makeApiTemplateBody());
-            this.logger.info(`  # API create start          ${blue('stackName=')}${stackName}`);
-            await this.waitForCreate(stackName);
-            this.logger.info(`  # API created               ${blue('stackName=')}${stackName}`);
-        } else {
-            const stackId = await this.updateStack(stackName, this.makeApiTemplateBody());
-            if (stackId) {
-                this.logger.info(`  # API update start          ${blue('stackName=')}${stackName}`);
-                await this.waitForUpdate(stackName);
-                this.logger.info(`  # API updated               ${blue('stackName=')}${stackName}`);
-            } else {
-                this.logger.info(`  # API No updates            ${blue('stackName=')}${stackName}`);
+  async deploy() {
+    await this.uploadSwaggerFiles();
+    const stackName = this.getStackName();
+    this.logger.debug(stackName);
+    const stack = await this.describeStack(stackName);
+    if (stack === undefined) {
+      await this.createStack(stackName, this.makeApiTemplateBody());
+      this.logger.info(`  # API create start          ${blue('stackName=')}${stackName}`);
+      await this.waitForCreate(stackName);
+      this.logger.info(`  # API created               ${blue('stackName=')}${stackName}`);
+    } else {
+      const stackId = await this.updateStack(stackName, this.makeApiTemplateBody());
+      if (stackId) {
+        this.logger.info(`  # API update start          ${blue('stackName=')}${stackName}`);
+        await this.waitForUpdate(stackName);
+        this.logger.info(`  # API updated               ${blue('stackName=')}${stackName}`);
+      } else {
+        this.logger.info(`  # API No updates            ${blue('stackName=')}${stackName}`);
+      }
+    }
+  }
+
+  async uploadSwaggerFiles() {
+    await Promise.all(
+      this.projectConfig.apis.map(apiName => this.uploadSwaggerFile(apiName))
+    );
+  }
+
+  async uploadSwaggerFile(apiName: string) {
+    const yaml = await this.mergeSwaggerFile(apiName);
+    utils.removeExamples(yaml);
+    const path = `${this.buidDir}/${apiName}.swagger.yaml`;
+    this.logger.debug(yaml);
+    const accountId = this.awsConfig.account_id;
+    const region = AWS.config.region;
+    if (!region) throw new Error();
+    let swagger = YAML.stringify(yaml);
+    swagger = swagger.replace(/\$\{AWS::AccountId\}/g, accountId);
+    swagger = swagger.replace(/\$\{AWS::Region\}/g, region);
+    this.logger.debug(`upload: ${path} => templates-${region}-${accountId}/apis/${apiName}.swagger.yaml`);
+    const params = {
+      Bucket: `templates-${region}-${accountId}`,
+      Key: `apis/${apiName}.swagger.yaml`,
+      Body: swagger
+    };
+    await this.s3.putObject(params).promise();
+    this.logger.info(`  # Uploaded   ${green(`s3://${params.Bucket}/${params.Key}`)}`);
+  }
+
+  makeApiTemplateBody(): string {
+    const resouces: any = {};
+    this.projectConfig.apis.map(apiName => {
+      resouces[apiName] = {
+        Type: 'AWS::ApiGateway::RestApi',
+        Properties: {
+          Body: {
+            'Fn::Transform': {
+              Name: 'AWS::Include',
+              Parameters: {
+                Location: `s3://templates-${this.awsConfig.region}-${this.awsConfig.account_id}/apis/${apiName}.swagger.yaml`
+              }
             }
+          }
         }
-    }
+      }
+    });
+    const body = {
+      AWSTemplateFormatVersion: '2010-09-09',
+      Description: `${this.projectConfig.name} API`,
+      Resources: resouces
+    };
+    this.logger.debug(YAML.stringify(body));
+    return YAML.stringify(body);
+  }
 
-    async uploadSwaggerFiles() {
-        await Promise.all(
-            this.projectConfig.apis.map(apiName => this.uploadSwaggerFile(apiName))
-        );
-    }
+  async deployApiStages(stageName: string) {
+    await Promise.all(
+      this.projectConfig.apis.map(apiName => this.deployApiStage(apiName, stageName))
+    );
+  }
 
-    async uploadSwaggerFile(apiName: string) {
-        const yaml = await this.mergeSwaggerFile(apiName);
-        utils.removeExamples(yaml);
-        const path = `${this.buidDir}/${apiName}.swagger.yaml`;
-        this.logger.debug(yaml);
-        const accountId = this.awsConfig.account_id;
-        const region = AWS.config.region;
-        if (!region) throw new Error();
-        let swagger = YAML.stringify(yaml);
-        swagger = swagger.replace(/\$\{AWS::AccountId\}/g, accountId);
-        swagger = swagger.replace(/\$\{AWS::Region\}/g, region);
-        this.logger.debug(`upload: ${path} => templates-${region}-${accountId}/apis/${apiName}.swagger.yaml`);
-        const params = {
-            Bucket: `templates-${region}-${accountId}`,
-            Key: `apis/${apiName}.swagger.yaml`,
-            Body: swagger
-        };
-        await this.s3.putObject(params).promise();
-        this.logger.info(`  # Uploaded   ${green(`s3://${params.Bucket}/${params.Key}`)}`);
-    }
+  async deployApiStage(apiName: string, stageName: string) {
+    const apiId = await this.getApiId(apiName);
+    const deploymentId = await this.createDeployment(apiId, stageName);
+    this.logger.debug(deploymentId);
+    await this.addPermissionToLambdaFunctions(apiId, stageName);
+    this.logger.info(`  # Stage deployed    ${blue('api=')}${apiName}, ${blue('stage=')}${stageName}`);
+  }
 
-    makeApiTemplateBody(): string {
-        const resouces: any = {};
-        this.projectConfig.apis.map(apiName => {
-            resouces[apiName] = {
-                Type: 'AWS::ApiGateway::RestApi',
-                Properties: {
-                    Body: {
-                        'Fn::Transform': {
-                            Name: 'AWS::Include',
-                            Parameters: {
-                                Location: `s3://templates-${this.awsConfig.region}-${this.awsConfig.account_id}/apis/${apiName}.swagger.yaml`
-                            }
-                        }
-                    }
-                }
-            }
-        });
-        const body = {
-            AWSTemplateFormatVersion: '2010-09-09',
-            Description: `${this.projectConfig.name} API`,
-            Resources: resouces
-        };
-        this.logger.debug(YAML.stringify(body));
-        return YAML.stringify(body);
-    }
+  async getApiId(apiName: string) {
+    const stackName = this.getStackName();
+    const params = {
+      StackName: stackName,
+      LogicalResourceId: apiName
+    };
+    const result = await this.cfn.describeStackResource(params).promise();
+    this.logger.debug(JSON.stringify(result, null, 2));
+    const apiId = result.StackResourceDetail?.PhysicalResourceId;
+    if (apiId === undefined) throw new Error(`apiId is unknown: ${apiName}`);
+    return apiId;
+  }
 
-    async deployApiStages(stageName: string) {
-        await Promise.all(
-            this.projectConfig.apis.map(apiName => this.deployApiStage(apiName, stageName))
-        );
-    }
+  async createDeployment(apiId: string, stageName: string) {
+    const params = {
+      restApiId: apiId,
+      stageName: stageName,
+      variables: {
+        version: stageName
+      }
+    };
+    const result = await this.apigateway.createDeployment(params).promise();
+    const id = result.id;
+    if (id === undefined) throw new Error(`deployment id is unknown: ${apiId}`);
+    return id;
+  }
 
-    async deployApiStage(apiName: string, stageName: string) {
-        const apiId = await this.getApiId(apiName);
-        const deploymentId = await this.createDeployment(apiId, stageName);
-        this.logger.debug(deploymentId);
-        await this.addPermissionToLambdaFunctions(apiId, stageName);
-        this.logger.info(`  # Stage deployed    ${blue('api=')}${apiName}, ${blue('stage=')}${stageName}`);
-    }
+  async addPermissionToLambdaFunctions(apiId: string, stageName: string) {
+    const functions = await this.getIntegrationFunctions(apiId);
+    await Promise.all(
+      functions.map(functionName => this.addPermissionToLambdaFunction(apiId, stageName, functionName))
+    );
+  }
 
-    async getApiId(apiName: string) {
-        const stackName = this.getStackName();
-        const params = {
-            StackName: stackName,
-            LogicalResourceId: apiName
-        };
-        const result = await this.cfn.describeStackResource(params).promise();
-        this.logger.debug(JSON.stringify(result, null, 2));
-        const apiId = result.StackResourceDetail?.PhysicalResourceId;
-        if (apiId === undefined) throw new Error(`apiId is unknown: ${apiName}`);
-        return apiId;
-    }
+  async getIntegrationFunctions(apiId: string): Promise<string[]> {
+    const resources = await this.getResources(apiId);
+    const paramsList = resources
+      .map(r => {
+        if (r.id === undefined) throw new Error('resouce id is undefined.');
+        const resourceId = r.id;
+        return Object.keys(r.resourceMethods || []).map(m => ({
+          restApiId: apiId,
+          resourceId,
+          httpMethod: m
+        }));
+      })
+      .flat();
+    const result = await Promise.all(
+      paramsList.map(params => this.apigateway.getIntegration(params).promise())
+    );
+    const functions = result
+      .map(i => {
+        if (i.uri === undefined) return undefined;
+        const m = i.uri.match(/\:function\:([a-z0-9_]+)[\:\/]/);
+        return m ? m[1] : undefined;
+      })
+      .filter(utils.notUndefined)
+      .filter((x, i, self) => self.indexOf(x) === i); // unique;
+    this.logger.debug(JSON.stringify(functions, null, 2));
+    return functions;
+  }
 
-    async createDeployment(apiId: string, stageName: string) {
-        const params = {
-            restApiId: apiId,
-            stageName: stageName,
-            variables: {
-                version: stageName
-            }
-        };
-        const result = await this.apigateway.createDeployment(params).promise();
-        const id = result.id;
-        if (id === undefined) throw new Error(`deployment id is unknown: ${apiId}`);
-        return id;
-    }
+  async getResources(apiId: string) {
+    const params = {
+      restApiId: apiId
+    };
+    const result = await this.apigateway.getResources(params).promise();
+    this.logger.debug(JSON.stringify(result, null, 2));
+    return result.items || [];
+  }
 
-    async addPermissionToLambdaFunctions(apiId: string, stageName: string) {
-        const functions = await this.getIntegrationFunctions(apiId);
-        await Promise.all(
-            functions.map(functionName => this.addPermissionToLambdaFunction(apiId, stageName, functionName))
-        );
-    }
-
-    async getIntegrationFunctions(apiId: string): Promise<string[]> {
-        const resources = await this.getResources(apiId);
-        const paramsList = resources
-            .map(r => {
-                if (r.id === undefined) throw new Error('resouce id is undefined.');
-                const resourceId = r.id; 
-                return Object.keys(r.resourceMethods || []).map(m => ({
-                    restApiId: apiId,
-                    resourceId,
-                    httpMethod: m
-                }));
-            })
-            .flat();
-        const result = await Promise.all(
-            paramsList.map(params => this.apigateway.getIntegration(params).promise())
-        );
-        const functions = result
-              .map(i => {
-                  if (i.uri === undefined) return undefined;
-                  const m = i.uri.match(/\:function\:([a-z0-9_]+)[\:\/]/);
-                  return m ? m[1] : undefined;
-              })
-              .filter(utils.notUndefined)
-              .filter((x, i, self) => self.indexOf(x) === i); // unique;
-        this.logger.debug(JSON.stringify(functions, null, 2));
-        return functions;
-    }
-
-    async getResources(apiId: string) {
-        const params = {
-            restApiId: apiId
-        };
-        const result = await this.apigateway.getResources(params).promise();
-        this.logger.debug(JSON.stringify(result, null, 2));
-        return result.items || [];
-    }
-
-    private getStackName() {
-        return `${this.projectConfig.name}-api`;
-    }
+  private getStackName() {
+    return `${this.projectConfig.name}-api`;
+  }
 
 }
