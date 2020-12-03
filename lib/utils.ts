a1cf7dd3c6b1fb226878d9ea0e53a89828a865f9
SHUJI Watanabe <watanabe.shuji@classmethod.jp>
2020-12-03 15:56:41 +0900

    format

--- lib/utils.ts
+++ lib/utils.ts
@@ -4,144 +4,144 @@ import { ProjectConfig, AwsConfig, LambdaConfig } from './types';
 import { Blob } from 'aws-sdk/lib/dynamodb/document_client';
 
 export const sleep = async (msec: number) => {
-    return new Promise((resolve, reject) => {
-        setTimeout(() => {
-            resolve();
-        }, msec);
-    })
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve();
+    }, msec);
+  })
 }
 
-export const loadYaml = (path: string, transformer?: Function): any|undefined => {
-    try {
-        const doc = readFileSync(path, 'utf8');
-        if (transformer) {
-            return YAML.parse(transformer.call(transformer, doc));
-        } else {
-            return YAML.parse(doc);
-        }
-    } catch (err) {
-        return undefined;
+export const loadYaml = (path: string, transformer?: Function): any | undefined => {
+  try {
+    const doc = readFileSync(path, 'utf8');
+    if (transformer) {
+      return YAML.parse(transformer.call(transformer, doc));
+    } else {
+      return YAML.parse(doc);
     }
+  } catch (err) {
+    return undefined;
+  }
 }
 
 export const loadProjectConfig = async (env: string, awsConfig: AwsConfig): Promise<ProjectConfig> => {
-    const transformer = (doc: string): string => {
-        let tmp = doc.replace(/\$\{AWS::AccountId\}/g, awsConfig.account_id);
-        tmp = tmp.replace(/\$\{AWS::Region\}/g, awsConfig.region);
-        if (doc === tmp) return tmp;
-        return transformer(tmp);
-    };
-    const config = loadYaml(`./conf/project.yaml`, transformer);
-    const envConf = loadYaml(`./conf/${env}/project.yaml`, transformer);
-    if (envConf) {
-        Object.keys(envConf).map(key => {
-            config[key] = envConf[key];
-        });
-    }
-    const functionsEnvConf = loadYaml(`./conf/${env}/functions.yaml`, transformer);
-    if (functionsEnvConf) {
-        const functionsConf = config.functions.configuration;
-        Object.keys(functionsEnvConf).map(key => {
-            switch (key) {
-                case 'environment':
-                    Object.keys(functionsEnvConf[key]).map(envKey => {
-                        functionsConf.environment[envKey] = functionsEnvConf[key][envKey];
-                    });                            
-                    break;
-                case 'layers':
-                    functionsConf.layers = functionsConf.layers.concat(functionsEnvConf.layers);
-                    break;
-                default:
-                    functionsConf[key] = functionsEnvConf[key];
-                    break;
-            }
-        });
-    }
-    return config as ProjectConfig;
+  const transformer = (doc: string): string => {
+    let tmp = doc.replace(/\$\{AWS::AccountId\}/g, awsConfig.account_id);
+    tmp = tmp.replace(/\$\{AWS::Region\}/g, awsConfig.region);
+    if (doc === tmp) return tmp;
+    return transformer(tmp);
+  };
+  const config = loadYaml(`./conf/project.yaml`, transformer);
+  const envConf = loadYaml(`./conf/${env}/project.yaml`, transformer);
+  if (envConf) {
+    Object.keys(envConf).map(key => {
+      config[key] = envConf[key];
+    });
+  }
+  const functionsEnvConf = loadYaml(`./conf/${env}/functions.yaml`, transformer);
+  if (functionsEnvConf) {
+    const functionsConf = config.functions.configuration;
+    Object.keys(functionsEnvConf).map(key => {
+      switch (key) {
+        case 'environment':
+          Object.keys(functionsEnvConf[key]).map(envKey => {
+            functionsConf.environment[envKey] = functionsEnvConf[key][envKey];
+          });
+          break;
+        case 'layers':
+          functionsConf.layers = functionsConf.layers.concat(functionsEnvConf.layers);
+          break;
+        default:
+          functionsConf[key] = functionsEnvConf[key];
+          break;
+      }
+    });
+  }
+  return config as ProjectConfig;
 }
 
 export const getAwsConfig = async (env: string): Promise<AwsConfig | undefined> => {
-    return loadYaml(`./conf/${env}/aws.yaml`);
+  return loadYaml(`./conf/${env}/aws.yaml`);
 }
 
 export const cleanupDir = async (dirPath: string) => {
-    try {
-        await fs.rmdir(dirPath, { recursive: true });
-    } catch(err) {
-        if (err.code !== 'ENOENT' && err.code !== 'ENOTDIR') throw err;
-    }
-    await fs.mkdir(dirPath, { recursive: true });
+  try {
+    await fs.rmdir(dirPath, { recursive: true });
+  } catch (err) {
+    if (err.code !== 'ENOENT' && err.code !== 'ENOTDIR') throw err;
+  }
+  await fs.mkdir(dirPath, { recursive: true });
 }
 
 export const toFunctionName = (func: string): string => {
-    if (func.indexOf('/') === 0) return func;
-    return func.split('/').join('_');
+  if (func.indexOf('/') === 0) return func;
+  return func.split('/').join('_');
 }
 
 export const toFunctionPath = (functionName: string): string => {
-    if (functionName.indexOf('/') !== -1) return functionName;
-    return functionName.replace('_', '/');
+  if (functionName.indexOf('/') !== -1) return functionName;
+  return functionName.replace('_', '/');
 }
 
 export const listFunctions = (paths: string[]) => {
-    return paths.map(
-            path => readdirSync(`./functions/${path}`).map(d => `${path}/${d}`)
-        ).flat()
-         .filter(path => existsSync(`./functions/${path}/function.yaml`));
+  return paths.map(
+    path => readdirSync(`./functions/${path}`).map(d => `${path}/${d}`)
+  ).flat()
+    .filter(path => existsSync(`./functions/${path}/function.yaml`));
 }
 
 export const payloadToObject = (payload: Buffer | Uint8Array | Blob | string | undefined) => {
-    if (payload === undefined) return undefined;
-    return JSON.parse(payload.toString());
+  if (payload === undefined) return undefined;
+  return JSON.parse(payload.toString());
 }
 
 export const removeExamples = (obj: any) => {
-    const rm = (obj: any) => {
-        Object.keys(obj).forEach(key => {
-            if (key == 'example') {
-                delete obj[key];
-            } else if (typeof obj[key] === 'object') {
-                rm(obj[key]);
-            }
-        });
-    };
-    Object.keys(obj).forEach(key => rm(obj[key]));
+  const rm = (obj: any) => {
+    Object.keys(obj).forEach(key => {
+      if (key == 'example') {
+        delete obj[key];
+      } else if (typeof obj[key] === 'object') {
+        rm(obj[key]);
+      }
+    });
+  };
+  Object.keys(obj).forEach(key => rm(obj[key]));
 }
 
 export const notUndefined = <T>(value: T | undefined): value is T => {
-    return value !== undefined;
+  return value !== undefined;
 }
 
 export const awsConfigTransformer = (awsConfig: AwsConfig) => {
-    const replace = (doc: string): string => {
-        let tmp = doc.replace(/\$\{AWS::AccountId\}/g, awsConfig.account_id);
-        tmp = tmp.replace(/\$\{AWS::Region\}/g, awsConfig.region);
-        if (doc === tmp) return tmp;
-        return replace(tmp);
-    }
-    return (doc: string) => {
-        return replace(doc);
-    };
+  const replace = (doc: string): string => {
+    let tmp = doc.replace(/\$\{AWS::AccountId\}/g, awsConfig.account_id);
+    tmp = tmp.replace(/\$\{AWS::Region\}/g, awsConfig.region);
+    if (doc === tmp) return tmp;
+    return replace(tmp);
+  }
+  return (doc: string) => {
+    return replace(doc);
+  };
 }
 
 export const mergeExtraVars = (payload: any, extraVars: { [key: string]: any }) => {
-    Object.keys(extraVars).forEach(key => {
-        if (key.indexOf('.') == -1) {
-            payload[key] = extraVars[key];
-        } else {
-            const keys = key.split('.');
-            payload[keys[0]][keys[1]] = extraVars[key];
-        }
-    });
-    return payload;
+  Object.keys(extraVars).forEach(key => {
+    if (key.indexOf('.') == -1) {
+      payload[key] = extraVars[key];
+    } else {
+      const keys = key.split('.');
+      payload[keys[0]][keys[1]] = extraVars[key];
+    }
+  });
+  return payload;
 }
 
 export const copyConfig = <T>(source: T): T => {
-    const yamlStr = YAML.stringify(source);
-    return YAML.parse(yamlStr) as T;
+  const yamlStr = YAML.stringify(source);
+  return YAML.parse(yamlStr) as T;
 }
 
 export const chunkArray = <T>(array: T[], len = 1): T[][] => {
-    const chunk = Array(Math.ceil(array.length / len));
-    return Array.from(chunk, (x, i) => array.slice(i * len, i * len + len))
+  const chunk = Array(Math.ceil(array.length / len));
+  return Array.from(chunk, (x, i) => array.slice(i * len, i * len + len))
 }
